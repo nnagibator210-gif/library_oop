22cc74ba1a57469a82efc10bf20580ee689d20e9
Student <student@atyrau.itstep>
2026-04-28 21:17:29 +0600

    Добавлены Magazine и main

--- main.py
+++ main.py
@@ -1,4 +1,27 @@
 from book import Book
+from magazine import Magazine
 
-book = Book("Война и мир", "Лев Толстой", 1869, "978-5-389-06254-2")
-print(book.get_info())
+
+book = Book(
+    "Война и мир",
+    "Лев Толстой",
+    1869,
+    "978-5-389-06254-2",
+    1225
+)
+
+magazine = Magazine(
+    "National Geographic",
+    "Сьюзан Голдберг",
+    2021,
+    8,
+    "Наука"
+)
+
+publications = [book, magazine]
+
+for publication in publications:
+    print(publication.get_info())
+
+print(book.read_pages(100))
+print(book.read_pages(50))
